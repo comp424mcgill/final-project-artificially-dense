c49b49d5436443d8617e9468d09cf42a19150a71
cndpea <97372902+cndpea@users.noreply.github.com>
2022-04-07 14:40:35 -0400

    Update student_world.py
    removed unecessary functions

--- agents/student_world.py
+++ agents/student_world.py
@@ -13,57 +13,17 @@ import sys
 class StudentWorld:
     def __init__(
             self,
-            player_1="random_agent",
-            player_2="random_agent",
-            board_size=None,
+            board=board,
+            player_1_position=position1,
+            player_2_position=position2,
+            max_step=max_step,
     ):
         """
-        Initialize the game world
-
-        Parameters
-        ----------
-        player_1: str
-            The registered class of the first player
-        player_2: str
-            The registered class of the second player
-        board_size: int
-            The size of the board. If None, board_size = a number between MIN_BOARD_SIZE and MAX_BOARD_SIZE
+        Initialize the world to the board given
         """
-        # Two players
-        logger.info("Initialize the game world")
         # Load agents as defined in decorators
-        self.player_1_name = player_1
-        self.player_2_name = player_2
-        if player_1 not in AGENT_REGISTRY:
-            raise ValueError(
-                f"Agent '{player_1}' is not registered. {AGENT_NOT_FOUND_MSG}"
-            )
-        if player_2 not in AGENT_REGISTRY:
-            raise ValueError(
-                f"Agent '{player_2}' is not registered. {AGENT_NOT_FOUND_MSG}"
-            )
-
-        p0_agent = AGENT_REGISTRY[player_1]
-        p1_agent = AGENT_REGISTRY[player_2]
-        logger.info(f"Registering p0 agent : {player_1}")
-        self.p0 = p0_agent()
-        logger.info(f"Registering p1 agent : {player_2}")
-        self.p1 = p1_agent()
-
-        # check autoplay
-        if autoplay:
-            if not self.p0.autoplay or not self.p1.autoplay:
-                raise ValueError(
-                    f"Autoplay mode is not supported by one of the agents ({self.p0} -> {self.p0.autoplay}, {self.p1} -> {self.p1.autoplay}). Please set autoplay=True in the agent class."
-                )
-
-        self.player_names = {PLAYER_1_ID: PLAYER_1_NAME, PLAYER_2_ID: PLAYER_2_NAME}
-        self.dir_names = {
-            DIRECTION_UP: DIRECTION_UP_NAME,
-            DIRECTION_RIGHT: DIRECTION_RIGHT_NAME,
-            DIRECTION_DOWN: DIRECTION_DOWN_NAME,
-            DIRECTION_LEFT: DIRECTION_LEFT_NAME,
-        }
+        self.p1_pos = player_1_position
+        self.p2_pos = player_2_position
 
         # Moves (Up, Right, Down, Left)
         self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
@@ -71,62 +31,18 @@ class StudentWorld:
         # Opposite Directions
         self.opposites = {0: 2, 1: 3, 2: 0, 3: 1}
 
-        if board_size is None:
-            # Random chessboard size
-            self.board_size = np.random.randint(MIN_BOARD_SIZE, MAX_BOARD_SIZE)
-            logger.info(
-                f"No board size specified. Randomly generating size : {self.board_size}x{self.board_size}"
-            )
-        else:
-            self.board_size = board_size
-            logger.info(f"Setting board size to {self.board_size}x{self.board_size}")
-
         # Index in dim2 represents [Up, Right, Down, Left] respectively
         # Record barriers and boarders for each block
-        self.chess_board = np.zeros((self.board_size, self.board_size, 4), dtype=bool)
-
-        # Set borders
-        self.chess_board[0, :, 0] = True
-        self.chess_board[:, 0, 3] = True
-        self.chess_board[-1, :, 2] = True
-        self.chess_board[:, -1, 1] = True
+        self.chess_board = deepcopy(board)
 
         # Maximum Steps
-        self.max_step = (self.board_size + 1) // 2
+        self.max_step = max_step
 
-        # Random barriers (symmetric)
-        for _ in range(self.max_step):
-            pos = np.random.randint(0, self.board_size, size=2)
-            r, c = pos
-            dir = np.random.randint(0, 4)
-            while self.chess_board[r, c, dir]:
-                pos = np.random.randint(0, self.board_size, size=2)
-                r, c = pos
-                dir = np.random.randint(0, 4)
-            anti_pos = self.board_size - 1 - pos
-            anti_dir = self.opposites[dir]
-            anti_r, anti_c = anti_pos
-            self.set_barrier(r, c, dir)
-            self.set_barrier(anti_r, anti_c, anti_dir)
-
-        # Random start position (symmetric but not overlap)
-        self.p0_pos = np.random.randint(0, self.board_size, size=2)
-        self.p1_pos = self.board_size - 1 - self.p0_pos
-        while np.array_equal(self.p0_pos, self.p1_pos):
-            self.p0_pos = np.random.randint(0, self.board_size, size=2)
-            self.p1_pos = self.board_size - 1 - self.p0_pos
         # Whose turn to step
         self.turn = 0
 
         # Check initialization
         self.initial_end, _, _ = self.check_endgame()
-
-        # Time taken by each player
-        self.p0_time = 0
-        self.p1_time = 0
-
-        # Cache to store and use the data
-        self.results_cache = ()
 
     def get_current_player(self):
         """
@@ -140,20 +56,6 @@ class StudentWorld:
             return self.p0, self.p0_pos, self.p1_pos
         else:
             return self.p1, self.p1_pos, self.p0_pos
-
-    def update_player_time(self, time_taken):
-        """
-        Update the time taken by the player
-
-        Parameters
-        ----------
-        time_taken : float
-            Time taken by the player
-        """
-        if not self.turn:
-            self.p0_time += time_taken
-        else:
-            self.p1_time += time_taken
 
     def step(self):
         """
@@ -226,57 +128,8 @@ class StudentWorld:
         self.turn = 1 - self.turn
 
         results = self.check_endgame()
-        self.results_cache = results
 
         return results
-
-    def check_valid_step(self, start_pos, end_pos, barrier_dir):
-        """
-        Check if the step the agent takes is valid (reachable and within max steps).
-
-        Parameters
-        ----------
-        start_pos : tuple
-            The start position of the agent.
-        end_pos : np.ndarray
-            The end position of the agent.
-        barrier_dir : int
-            The direction of the barrier.
-        """
-        # Endpoint already has barrier or is boarder
-        r, c = end_pos
-        if self.chess_board[r, c, barrier_dir]:
-            return False
-        if np.array_equal(start_pos, end_pos):
-            return True
-
-        # Get position of the adversary
-        adv_pos = self.p0_pos if self.turn else self.p1_pos
-
-        # BFS
-        state_queue = [(start_pos, 0)]
-        visited = {tuple(start_pos)}
-        is_reached = False
-        while state_queue and not is_reached:
-            cur_pos, cur_step = state_queue.pop(0)
-            r, c = cur_pos
-            if cur_step == self.max_step:
-                break
-            for dir, move in enumerate(self.moves):
-                if self.chess_board[r, c, dir]:
-                    continue
-
-                next_pos = cur_pos + move
-                if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
-                    continue
-                if np.array_equal(next_pos, end_pos):
-                    is_reached = True
-                    break
-
-                visited.add(tuple(next_pos))
-                state_queue.append((next_pos, cur_step + 1))
-
-        return is_reached
 
     def check_endgame(self):
         """
@@ -421,6 +274,7 @@ class StudentWorld:
 
     def random_move(self, my_pos, adv_pos):
         """
+        ** Reedited version
         Output:
         - a random move from the current position
         """
