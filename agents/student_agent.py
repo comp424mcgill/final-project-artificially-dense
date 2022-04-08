6ad6e3d7d91b3caf7485600e698f7aefb530edc5
cndpea <97372902+cndpea@users.noreply.github.com>
2022-04-08 02:28:33 -0400

    draft mcts

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -3,7 +3,7 @@ from agents.agent import Agent
 from store import register_agent
 import sys
 from copy import deepcopy
-from student_minimax import student_minimax
+from agents.student_world import StudentWorld
 
 
 @register_agent("student_agent")
@@ -23,7 +23,60 @@ class StudentAgent(Agent):
             "l": 3,
         }
 
-    def step(self, chess_board, my_pos, adv_pos, max_step):
+    @staticmethod
+    def random_move(chess_board, my_position, adv_position, max_step):
+        """
+        ** Reedited version
+        Output:
+        - a random move from the current position
+        """
+        # Moves (Up, Right, Down, Left)
+        ori_pos = deepcopy(my_position)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        steps = np.random.randint(0, max_step + 1)  # a random number of steps
+
+        # Random Walk
+        for _ in range(steps):
+            my_x, my_y = my_position
+            rand_step = np.random.randint(0, 4)
+            rand_step_x, rand_step_y = moves[rand_step]
+            my_position = (my_x + rand_step_x, my_y + rand_step_y)
+
+            # Special Case enclosed by Adversary
+            k = 0
+            # If there's a wall or there's an adversary at the new place, change step
+            while chess_board[my_x, my_y, rand_step] or my_position == adv_position:
+                k += 1
+                # terminating condition
+                if k > 300:
+                    break
+                rand_step = np.random.randint(0, 4)
+                rand_step_x, rand_step_y = moves[rand_step]
+                my_pos = (my_x + rand_step_x, my_y + rand_step_y)
+
+            if k > 300:
+                my_pos = ori_pos
+                break
+
+        # Put Barrier
+        rand_wall = np.random.randint(0, 4)
+        my_x, my_y = my_position
+        while chess_board[my_x, my_y, rand_wall]:
+            rand_wall = np.random.randint(0, 4)
+
+        return my_position, rand_wall
+
+    @staticmethod
+    def set_barrier(chess_board, x, y, dir):
+        # Set the barrier to True
+        chess_board[x, y, dir] = True
+        # Set the opposite barrier to True
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        move = moves[dir]
+        chess_board[x + move[0], y + move[1], self.opposites[dir]] = True
+
+    @staticmethod
+    def step(chess_board, my_pos, adv_pos, max_step):
         """
         Implement the step function of your agent here.
         You can use the following variables to access the chess board:
@@ -39,22 +92,29 @@ class StudentAgent(Agent):
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
 
-        number_rand_actions = 8  # can change
-        number_rand_trials = 8  # can change
-
-        highest_prob = 0
-        highest_prob_action = (0, 0), 0
-
-        for _ in range(number_rand_actions):
-            ori_board = deepcopy(chess_board)
-            ori_pos = deepcopy(my_pos)
-            sum = 0
-            (rand_x, rand_y), direction = self.random_move(ori_board, ori_pos, adv_pos, max_step)
-            ori_board[rand_x, rand_y, direction] = True
-            for _ in range(number_rand_trials):
-                sum += mcts(ori_board, (rand_x, rand_y), adv_pos, max_step - 1)
-            if sum / number_rand_trials > highest_prob:
-                highest_prob_action = rand_move
-
-        return highest_prob_action
-        return (0, 0), 1
+        # number_rand_actions = 1  # can change
+        # number_rand_trials = 1  # can change
+        #
+        # highest_prob = 0
+        # highest_prob_action = (0, 0), 0
+        #
+        # for _ in range(number_rand_actions):
+        #     ori_board = deepcopy(chess_board)
+        #     ori_pos = deepcopy(my_pos)
+        #     sum = 0
+        #     (rand_x, rand_y), direction = random_move(ori_board, ori_pos, adv_pos, max_step)
+        #     ori_board[rand_x, rand_y, direction] = True
+        #     for _ in range(number_rand_trials):
+        #         sum += mcts(ori_board, (rand_x, rand_y), adv_pos, max_step - 1)
+        #     if sum / number_rand_trials > highest_prob:
+        #         highest_prob_action = (rand_x, rand_y), direction
+        #
+        # return highest_prob_action
+        print("hi1")
+        exp = StudentWorld(chess_board, my_pos, adv_pos, max_step)
+        print("hi2")
+        action = exp.step
+        print("hi3")
+        print(action)
+        print("hi")
+        return action
--- agents/student_world.py
+++ agents/student_world.py
@@ -10,12 +10,19 @@ from constants import *
 import sys
 
 
-class StudentWorld:
+class StudentWorld(Agent):
     def __init__(self, board, our_position, adv_position, max_step):
         """
         Initialize the world to the board given
         """
-        # Load agents as defined in decorators
+        super(StudentWorld, self).__init__()
+        # Make a copy of the given information - info to not be modified
+        self.chess_board_copy = deepcopy(board)
+        self.our_pos_copy = deepcopy(our_position)
+        self.adv_pos_copy = deepcopy(adv_position)
+
+        # Load info as given in decorators
+        self.chess_board = board
         self.our_pos = our_position
         self.adv_pos = adv_position
 
@@ -25,104 +32,123 @@ class StudentWorld:
         # Opposite Directions
         self.opposites = {0: 2, 1: 3, 2: 0, 3: 1}
 
-        # Index in dim2 represents [Up, Right, Down, Left] respectively
-        # Record barriers and boarders for each block
-        self.chess_board = deepcopy(board)
-
         # Maximum Steps
         self.max_step = max_step
 
         # Whose turn to step
-        self.turn = 0
+        self.turn = False  # true being our turn, false being opponent's turn
 
         # Check initialization
         self.initial_end, _, _ = self.check_endgame()
 
         # Record the winner
-        self.winner = -1  # 1 being we win, 0.5 being tie, 0 being loss
+        self.winner = -1  # 1 being we win, 0.5 being tie, 0 being we lose
 
-    def get_current_player(self):
-        """
-        Get the positions of the current player
+        # Count how deep we want to go in the random search
+        self.depth = 1
 
-        Returns
-        -------
-        tuple of (current_player_obj, current_player_pos, adversary_player_pos)
-        """
-        if not self.turn:
-            return self.p0, self.p0_pos, self.p1_pos
-        else:
-            return self.p1, self.p1_pos, self.p0_pos
+        # Moves (Up, Right, Down, Left)
+        self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
-    def step(self):
+    def step(self, chess_board, my_pos, adv_pos, max_step):
         """
-        Take a step in the game world.
-        Runs the agents' step function and update the game board accordingly.
-        If the agents' step function raises an exception, the step will be replaced by a Random Walk.
+        Implement the step function of your agent here.
+        You can use the following variables to access the chess board:
+        - chess_board: a numpy array of shape (x_max, y_max, 4)
+        - my_pos: a tuple of (x, y)
+        - adv_pos: a tuple of (x, y)
+        - max_step: an integer
+
+        You should return a tuple of ((x, y), dir),
+        where (x, y) is the next position of your agent and dir is the direction of the wall
+        you want to put on.
+
+        Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
+        """
+
+        number_rand_actions = 1  # can change
+        number_rand_trials = 1  # can change
+
+        highest_prob = 0
+        highest_prob_action = (0, 0), 0
+
+        for _ in range(number_rand_actions):
+            # Start from the same original board
+            self.chess_board = deepcopy(self.board_copy)
+            self.our_pos = deepcopy(self.our_pos_copy)
+            self.adv_pos = deepcopy(self.adv_pos_copy)
+            self.winner = -1
+            sum = 0
+            (rand_x, rand_y), direction = self.random_move(self.our_pos, self.adv_pos)
+            ended, score1, score2 = self.check_endgame()
+            if not ended:
+                for _ in range(number_rand_trials):
+                    sum += self.run_experiment()
+                if sum / number_rand_trials > highest_prob:
+                    highest_prob_action = (rand_x, rand_y), direction
+            else:
+                if self.winner > highest_prob:
+                    highest_prob_action = (rand_x, rand_y), direction
+
+            if highest_prob_action == 1:
+                break
+
+        return highest_prob_action
+
+    def run_experiment(self):
+        """
+        Repeatedly run random walk for both players;
+        If the game ended, return the winner; else, use heuristics (or minimax)
+        Output:
+        - Winning value
+        """
+        d = 0
+        ended, score1, score2 = self.increase_depth()
+        while (not ended) or (d < self.depth):
+            ended, score1, score2 = self.increase_depth()
+            d += 1
+        if not ended:
+            self.winner = heuristics()
+
+        return self.winner
+
+    def heuristics(self):
+        """
+        Return an approximate winning value
+        """
+        self.winner = 1
+        return self.winner
+
+    def increase_depth(self):
+        """
+        Take a Random Walk.
 
         Returns
         -------
         results: tuple
             The results of the step containing (is_endgame, player_1_score, player_2_score)
         """
-        cur_player, cur_pos, adv_pos = self.get_current_player()
-
-        try:
-            # Run the agents step function
-            start_time = time()
-            next_pos, dir = cur_player.step(
-                deepcopy(self.chess_board),
-                tuple(cur_pos),
-                tuple(adv_pos),
-                self.max_step,
-            )
-            self.update_player_time(time() - start_time)
-
-            next_pos = np.asarray(next_pos, dtype=cur_pos.dtype)
-            if not self.check_boundary(next_pos):
-                raise ValueError("End position {} is out of boundary".format(next_pos))
-            if not 0 <= dir <= 3:
-                raise ValueError(
-                    "Barrier dir should reside in [0, 3], but your dir is {}".format(
-                        dir
-                    )
-                )
-            if not self.check_valid_step(cur_pos, next_pos, dir):
-                raise ValueError(
-                    "Not a valid step from {} to {} and put barrier at {}, with max steps = {}".format(
-                        cur_pos, next_pos, dir, self.max_step
-                    )
-                )
-        except BaseException as e:
-            ex_type = type(e).__name__
-            if (
-                    "SystemExit" in ex_type and isinstance(cur_player, HumanAgent)
-            ) or "KeyboardInterrupt" in ex_type:
-                sys.exit(0)
-            print(
-                "An exception raised. The traceback is as follows:\n{}".format(
-                    traceback.format_exc()
-                )
-            )
-            print("Execute Random Walk!")
-            next_pos, dir = self.random_walk(tuple(cur_pos), tuple(adv_pos))
-            next_pos = np.asarray(next_pos, dtype=cur_pos.dtype)
-
-        # Print out each step
-        # print(self.turn, next_pos, dir)
-
-        if not self.turn:
-            self.p0_pos = next_pos
+        if self.turn:
+            our_position = self.our_pos
+            adv_position = self.adv_pos
         else:
-            self.p1_pos = next_pos
-        # Set the barrier to True
-        r, c = next_pos
-        self.set_barrier(r, c, dir)
+            our_position = self.adv_pos
+            adv_position = self.our_pos
 
-        # Change turn
-        self.turn = 1 - self.turn
+        our_position, dir = self.random_move(our_position, adv_position)
+
+        if self.turn:
+            self.our_pos = our_position
+        else:
+            self.adv_pos = our_position
+        # Set the barrier to True
+        pos_x, pos_y = our_position
+        self.set_barrier(pos_x, pos_y, dir)
 
         results = self.check_endgame()
+
+        # Change turn
+        self.turn = not self.turn
 
         return results
 
@@ -134,16 +160,16 @@ class StudentWorld:
         -------
         is_endgame : bool
             Whether the game ends.
-        player_1_score : int
-            The score of player 1.
-        player_2_score : int
-            The score of player 2.
+        our_score : int
+            Our score.
+        adv_score : int
+            The adversary's score.
         """
         # Union-Find
         father = dict()
-        for r in range(self.board_size):
-            for c in range(self.board_size):
-                father[(r, c)] = (r, c)
+        for pos_x in range(self.board_size):
+            for pos_y in range(self.board_size):
+                father[(pos_x, pos_y)] = (pos_x, pos_y)
 
         def find(pos):
             if father[pos] != pos:
@@ -153,70 +179,69 @@ class StudentWorld:
         def union(pos1, pos2):
             father[pos1] = pos2
 
-        for r in range(self.board_size):
-            for c in range(self.board_size):
-                for dir, move in enumerate(
-                        self.moves[1:3]
-                ):  # Only check down and right
-                    if self.chess_board[r, c, dir + 1]:
+        for pos_x in range(self.board_size):
+            for pos_y in range(self.board_size):
+                for dir, move in enumerate(self.moves[1:3]):  # Only check down and right
+                    # Check if there is a wall between current position and down/right
+                    if self.chess_board[pos_x, pos_y, dir + 1]:
                         continue
-                    pos_a = find((r, c))
-                    pos_b = find((r + move[0], c + move[1]))
-                    if pos_a != pos_b:
-                        union(pos_a, pos_b)
+                    # If no wall, put the two into the same disjoint set
+                    root_a = find((pos_x, pos_y))
+                    root_b = find((pos_x + move[0], pos_y + move[1]))
+                    if root_a != root_b:
+                        union(root_a, root_b)
 
-        for r in range(self.board_size):
-            for c in range(self.board_size):
-                find((r, c))
-        p0_r = find(tuple(self.p0_pos))
-        p1_r = find(tuple(self.p1_pos))
-        p0_score = list(father.values()).count(p0_r)
-        p1_score = list(father.values()).count(p1_r)
-        if p0_r == p1_r:
-            return False, p0_score, p1_score
-        player_win = None
-        win_blocks = -1
+        for pos_x in range(self.board_size):
+            for pos_y in range(self.board_size):
+                find((pos_x, pos_y))  # Path compression of union find
+        our_r = find(tuple(self.our_pos))
+        adv_r = find(tuple(self.adv_pos))
+        our_score = list(father.values()).count(our_r)
+        adv_score = list(father.values()).count(adv_r)
+        if our_r == adv_r:
+            return False, our_score, adv_score
+
         if our_score > adv_score:
-            self.winner = 1
+            if self.turn:
+                self.winner = 1
+            else:
+                self.winner = 0
         elif p0_score < p1_score:
-            self.winner = 0
+            if self.turn:
+                self.winner = 0
+            else:
+                self.winner = 1
         else:
             self.winner = 0.5
-        return True, p0_score, p1_score
+        return True, our_score, adv_score
 
-    def check_boundary(self, pos):
-        r, c = pos
-        return 0 <= r < self.board_size and 0 <= c < self.board_size
-
-    def set_barrier(self, r, c, dir):
+    def set_barrier(self, x, y, dir):
         # Set the barrier to True
-        self.chess_board[r, c, dir] = True
+        self.chess_board[x, y, dir] = True
         # Set the opposite barrier to True
         move = self.moves[dir]
-        self.chess_board[r + move[0], c + move[1], self.opposites[dir]] = True
+        self.chess_board[x + move[0], y + move[1], self.opposites[dir]] = True
 
-    def random_move(self, my_pos, adv_pos):
+    def random_move(self, my_position, adv_position):
         """
         ** Reedited version
         Output:
         - a random move from the current position
         """
-        # Moves (Up, Right, Down, Left)
-        ori_pos = deepcopy(my_pos)
-        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        ori_pos = deepcopy(my_position)
         steps = np.random.randint(0, self.max_step + 1)  # a random number of steps
 
         # Random Walk
         for _ in range(steps):
-            my_x, my_y = my_pos
+            my_x, my_y = my_position
             rand_step = np.random.randint(0, 4)
             rand_step_x, rand_step_y = self.moves[rand_step]
-            my_pos = (my_x + rand_step_x, my_y + rand_step_y)
+            my_position = (my_x + rand_step_x, my_y + rand_step_y)
 
             # Special Case enclosed by Adversary
             k = 0
             # If there's a wall or there's an adversary at the new place, change step
-            while self.chess_board[my_x, my_y, rand_step] or my_pos == adv_pos:
+            while self.chess_board[my_x, my_y, rand_step] or my_position == adv_position:
                 k += 1
                 # terminating condition
                 if k > 300:
@@ -231,11 +256,11 @@ class StudentWorld:
 
         # Put Barrier
         rand_wall = np.random.randint(0, 4)
-        my_x, my_y = my_pos
+        my_x, my_y = my_position
         while self.chess_board[my_x, my_y, rand_wall]:
             rand_wall = np.random.randint(0, 4)
 
-        return my_pos, rand_wall
+        return my_position, rand_wall
 
 
 if __name__ == "__main__":
